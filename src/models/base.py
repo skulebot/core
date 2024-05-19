6752ea2e02781667e386b03e6ddedcff306ddb68
aelkheir <90580077+aelkheir@users.noreply.github.com>
2024-05-19 19:19:17 +0300

    Add naming convention for constraints

--- src/models/base.py
+++ src/models/base.py
@@ -1,5 +1,14 @@
+from sqlalchemy import MetaData
 from sqlalchemy.orm import DeclarativeBase, MappedAsDataclass
 
 
 class Base(DeclarativeBase, MappedAsDataclass):
-    pass
+    metadata = MetaData(
+        naming_convention={
+            "ix": "%(column_0_label)s_idx",
+            "uq": "%(table_name)s_%(column_0_name)s_key",
+            "ck": "%(table_name)s_%(constraint_name)s_check",
+            "fk": "%(table_name)s_%(column_0_name)s_%(referred_table_name)s_fkey",
+            "pk": "%(table_name)s_pkey",
+        }
+    )
